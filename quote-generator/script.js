bdf1d87014b9b8caaecadd3d783a22958aa0c6bd
John McKie <john.mckie@remarkable.net>
2020-07-27 17:43:35 +0100

    some js for you

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,1 +1,47 @@
-console.log('here we are');
+const quoteContainer = document.querySelector('#quote-container');
+const quoteText= document.querySelector('#quote');
+const authorText = document.querySelector('#author');
+const twitterBtn = document.querySelector('#twitter');
+const newQuoteBtn = document.querySelector('#new-quote');
+
+
+// get quote from api
+async function getQuote() {
+    const proxyURL = 'https://cors-anywhere.herokuapp.com/';
+    const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyURL + apiURL);
+        const data = await response.json();
+        // if author is blank, add unknown
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // reduce font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+    } catch (error) {
+        getQuote();
+    }
+}
+
+// tweet quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterURL = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterURL, '_blank');
+
+}
+
+// event listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// on load
+getQuote();
