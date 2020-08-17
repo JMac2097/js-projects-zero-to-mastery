d9a8d1fa37efcbd96001b81e13caefd773af0a83
John McKie <john.mckie@remarkable.net>
2020-08-17 14:43:12 +0100

    finished up joke teller

--- picture-in-pictureAPI/script.js
+++ picture-in-pictureAPI/script.js
@@ -1,1 +1,29 @@
-console.log('here we are');
+const videoElement = document.querySelector('#video');
+const button = document.querySelector('#button');
+
+
+// Prompt user to select media strea, pass to video element, then play
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch(error) {
+        // catch errors
+        console.log('whoops error here: ', error)
+    }
+}
+
+button.addEventListener('click', async () => {
+    // disable button
+    button.disabled = true;
+    // start picture in picture
+    await videoElement.requestPictureInPicture();
+    // reset button
+    button.disabled = false;
+});
+
+selectMediaStream();
